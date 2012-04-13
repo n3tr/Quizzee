1f6fc2a68cfa27c1b81571dc0f651726bbf3218a
Jirat K <saakyz@gmail.com>
2012-04-14 06:14:46 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizzee::Application.config.secret_token = '93db2d0a13c6fc13ac2f9fd573eb1684ba5481531169a100955a7044b682dafdf426ba23cff56045c1c3406bb98ed0e87c3a217cab99735ab49e0c00d8db47c3'
